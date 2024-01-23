19f1c5c61f628283afb149e21f5bf007614d822c
Hanna Grehn <hannagrehn@gmail.com>
2024-01-23 12:10:01 +0100

    repos

---
+++ ShirtSkirt/Repositories/BaseRepo.cs
@@ -0,0 +1,88 @@
+﻿
+using ShirtSkirt.Contexts;
+using System.Diagnostics;
+using System.Linq.Expressions;
+
+namespace ShirtSkirt.Repositories;
+
+public abstract class BaseRepo<TEntity> where TEntity : class
+{
+    private readonly DataContext _context;
+
+    protected BaseRepo(DataContext context)
+    {
+        _context = context;
+    }
+
+    public virtual TEntity Create(TEntity entity)
+    {
+        try
+        {
+            _context.Set<TEntity>().Add(entity);
+            _context.SaveChanges();
+            return entity;
+        }
+        catch (Exception ex) { Debug.WriteLine("Noo :: " + ex.Message); }
+        return null!;
+    } 
+
+
+    public virtual IEnumerable<TEntity> GetAll()
+    {
+        try
+        {
+            return _context.Set<TEntity>().ToList();
+        }
+        catch (Exception ex) { Debug.WriteLine("Neein :: " + ex.Message); }
+        return null!;
+    }
+
+    public virtual TEntity GetOne(Expression<Func<TEntity, bool>> predicate)
+    {
+        try
+        {
+            return _context.Set<TEntity>().FirstOrDefault(predicate, null!);
+        }
+        catch (Exception ex) { Debug.WriteLine("Neein :: " + ex.Message); }
+        return null!;
+    }
+
+
+    public virtual TEntity Update(TEntity entity)
+    {
+        try
+        {
+            var entityToUpdate = _context.Set<TEntity>().Find(entity);
+            if (entityToUpdate != null)
+            {
+                entityToUpdate = entity;
+                _context.Set<TEntity>().Update(entityToUpdate);
+                _context.SaveChanges();
+
+                return entityToUpdate;
+            }
+
+        }
+        catch (Exception ex) { Debug.WriteLine("Neein :: " + ex.Message); }
+        return null!;
+    }
+
+
+    public virtual bool Delete(Expression<Func<TEntity, bool>> predicate)
+    {
+        try
+        {
+            var entity = _context.Set<TEntity>().FirstOrDefault(predicate);
+            if (entity != null)
+            {
+                _context.Set<TEntity>().Remove(entity);
+                _context.SaveChanges();
+
+                return true;
+            }
+
+        }
+        catch (Exception ex) { Debug.WriteLine("Neein :: " + ex.Message); }
+        return false;
+    }
+}
